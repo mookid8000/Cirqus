26087abbefc3fafcd560d1b4dfca8db58e0ccb66
Mogens Heller Grabe <mookid8000@gmail.com>
2014-08-13 23:06:17 +0200

    added NLog integration

---
+++ d60.Cirqus.NLog/NLogLoggerFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using d60.Cirqus.Logging;
+using NLog;
+using Logger = d60.Cirqus.Logging.Logger;
+
+namespace d60.Cirqus.NLog
+{
+    public class NLogLoggerFactory : CirqusLoggerFactory
+    {
+        public override Logger GetLogger(Type ownerType)
+        {
+            return new NLogLogger(ownerType);
+        }
+
+        class NLogLogger : Logger
+        {
+            readonly global::NLog.Logger _logger;
+            
+            public NLogLogger(Type ownerType)
+            {
+                _logger = LogManager.GetLogger(ownerType.FullName);
+            }
+            
+            public override void Debug(string message, params object[] objs)
+            {
+                _logger.Debug(message, objs);
+            }
+
+            public override void Info(string message, params object[] objs)
+            {
+                _logger.Info(message, objs);
+            }
+
+            public override void Warn(string message, params object[] objs)
+            {
+                _logger.Warn(message, objs);
+            }
+
+            public override void Error(string message, params object[] objs)
+            {
+                _logger.Error(message, objs);
+            }
+        }
+    }
+}
