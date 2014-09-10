56a64ec889e2482c53857116a68162092b81b30c
Mogens Heller Grabe <mookid8000@gmail.com>
2014-09-10 22:21:11 +0200

    default to true

--- d60.Cirqus.MsSql/Config/MsSqlConfigurationExtensions.cs
+++ d60.Cirqus.MsSql/Config/MsSqlConfigurationExtensions.cs
@@ -7,7 +7,7 @@ namespace d60.Cirqus.MsSql.Config
 {
     public static class MsSqlConfigurationExtensions
     {
-        public static void UseSqlServer(this EventStoreConfigurationBuilder builder, string connectionStringOrConnectionStringName, string tableName, bool automaticallyCreateSchema)
+        public static void UseSqlServer(this EventStoreConfigurationBuilder builder, string connectionStringOrConnectionStringName, string tableName, bool automaticallyCreateSchema = true)
         {
             if (builder == null) throw new ArgumentNullException("builder");
             if (connectionStringOrConnectionStringName == null) throw new ArgumentNullException("connectionStringOrConnectionStringName");
