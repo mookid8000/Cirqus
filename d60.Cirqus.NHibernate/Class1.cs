380cccf9a243110a85454c7c66805d4feec81e15
Mogens Heller Grabe <mookid8000@gmail.com>
2014-10-08 16:24:56 +0200

    laid ground for first contract test

---
+++ d60.Cirqus.NHibernate/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using d60.Cirqus.Events;
+using d60.Cirqus.Views.ViewManagers;
+
+namespace d60.Cirqus.NHibernate
+{
+    public class NHibernateViewManager<TViewInstance> : IViewManager<TViewInstance> where TViewInstance : class, IViewInstance, ISubscribeTo, new()
+    {
+        public long GetPosition(bool canGetFromCache = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispatch(IViewContext viewContext, IEnumerable<DomainEvent> batch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task WaitUntilProcessed(CommandProcessingResult result, TimeSpan timeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Purge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TViewInstance Load(string viewId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event ViewInstanceUpdatedHandler<TViewInstance> Updated;
+    }
+}
---
+++ d60.Cirqus.Tests/Contracts/Views/Factories/NHibernateViewManagerFactory.cs
@@ -0,0 +1,13 @@
+﻿using d60.Cirqus.NHibernate;
+using d60.Cirqus.Views.ViewManagers;
+
+namespace d60.Cirqus.Tests.Contracts.Views.Factories
+{
+    public class NHibernateViewManagerFactory : AbstractViewManagerFactory
+    {
+        protected override IViewManager<TViewInstance> CreateViewManager<TViewInstance>()
+        {
+            return new NHibernateViewManager<TViewInstance>();
+        }
+    }
+}
