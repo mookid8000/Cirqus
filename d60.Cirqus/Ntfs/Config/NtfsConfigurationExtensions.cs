3a619def5f911a92bfaf3f37979aa91ae2a5c605
Asger Hallas <ahl@asgerhallas.dk>
2014-10-22 21:25:05 +0200

    Pulled the ntfs store into core, as it has no dependencies

---
+++ d60.Cirqus/Ntfs/Config/NtfsConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using d60.Cirqus.Config.Configurers;
+using d60.Cirqus.Events;
+using d60.Cirqus.Ntfs.Events;
+
+namespace d60.Cirqus.Ntfs.Config
+{
+    public static class NtfsConfigurationExtensions
+    {
+        public static void UseFiles(this EventStoreConfigurationBuilder builder, string path)
+        {
+            if (builder == null) throw new ArgumentNullException("builder");
+            if (string.IsNullOrEmpty(path)) throw new ArgumentNullException("path");
+            
+            builder.Registrar.Register<IEventStore>(context => new NtfsEventStore(path));
+        }
+    }
+}
