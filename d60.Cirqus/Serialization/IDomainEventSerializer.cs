3ad67edf9477438f53f7bb5e437053c9dd85932d
Mogens Heller Grabe <mookid8000@gmail.com>
2014-10-28 17:47:58 +0100

    added (first version of) serializer interface

---
+++ d60.Cirqus/Serialization/IDomainEventSerializer.cs
@@ -0,0 +1,10 @@
+﻿using d60.Cirqus.Events;
+
+namespace d60.Cirqus.Serialization
+{
+    public interface IDomainEventSerializer
+    {
+        Event DoSerialize(DomainEvent e);
+        DomainEvent DoDeserialize(Event e);
+    }
+}
