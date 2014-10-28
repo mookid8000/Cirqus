4a3f583b589ef1a07c910cb7f33d28607ba95d3d
Asger Hallas <ahl@asgerhallas.dk>
2014-10-28 21:36:20 +0100

    Now with file too...

---
+++ d60.Cirqus/Events/Event.cs
@@ -0,0 +1,16 @@
+﻿using d60.Cirqus.Numbers;
+
+namespace d60.Cirqus.Events
+{
+    public class Event
+    {
+        public Event()
+        {
+            Meta = new Metadata();
+            Data = new byte[0];
+        }
+
+        public Metadata Meta { get; set; }
+        public virtual byte[] Data { get; set; }
+    }
+}
