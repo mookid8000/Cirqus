cd5f3bf28e0353779a0a87c1f33347d1c5be96bc
Mogens Heller Grabe <mookid8000@gmail.com>
2014-11-10 06:47:28 +0100

    created command classes

---
+++ d60.Cirqus/Commands/UpdateCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using d60.Cirqus.Aggregates;
+
+namespace d60.Cirqus.Commands
+{
+    public abstract class UpdateCommand<TAggregateRoot> : Command where TAggregateRoot : AggregateRoot, new()
+    {
+        protected UpdateCommand(string aggregateRootId)
+        {
+            if (aggregateRootId == null)
+                throw new ArgumentNullException("aggregateRootId", "You need to specify an aggregate root ID");
+
+            AggregateRootId = aggregateRootId;
+        }
+
+        public string AggregateRootId { get; private set; }
+
+        public override sealed void Execute(ICommandContext context)
+        {
+            var aggregateRootInstance = context.Load<TAggregateRoot>(AggregateRootId, createIfNotExists: false);
+
+            Execute(aggregateRootInstance);
+        }
+
+        public abstract void Execute(TAggregateRoot aggregateRoot);
+
+        public override string ToString()
+        {
+            return string.Format("{0} => {1} {2}", GetType().Name, typeof (TAggregateRoot).Name, AggregateRootId);
+        }
+    }
+}
