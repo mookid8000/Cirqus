6a747767c10924bb06b046fc9d810381ce02309e
Mogens Heller Grabe <mookid8000@gmail.com>
2014-11-14 13:45:25 +0100

    added comments

--- d60.Cirqus/Config/IDomainTypeNameMapper.cs
+++ d60.Cirqus/Config/IDomainTypeNameMapper.cs
@@ -7,7 +7,14 @@ namespace d60.Cirqus.Config
     /// </summary>
     public interface IDomainTypeNameMapper
     {
+        /// <summary>
+        /// Gets the type from the name. Throws an exception if the given name could not be mapped unambiguously to tye type of either an aggregate root or a doman event
+        /// </summary>
         Type GetType(string name);
+
+        /// <summary>
+        /// Gets the name from the type. Throws an exception if the given name could not be mapped in a way that can unambiguously be mapped back again
+        /// </summary>
         string GetName(Type type);
     }
 }
