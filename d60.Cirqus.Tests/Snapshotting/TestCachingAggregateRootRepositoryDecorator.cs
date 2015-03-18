16fe57561f7712653a442b51bb34372a2f1d0cb6
Mogens Heller Grabe <mookid8000@gmail.com>
2015-03-18 13:51:48 +0100

    added simple caching aggregate root repo perf measurement test

---
+++ d60.Cirqus.Tests/Snapshotting/TestCachingAggregateRootRepositoryDecorator.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using d60.Cirqus.Aggregates;
+using d60.Cirqus.Commands;
+using d60.Cirqus.Config;
+using d60.Cirqus.Events;
+using d60.Cirqus.Extensions;
+using d60.Cirqus.MongoDb.Events;
+using d60.Cirqus.Serialization;
+using d60.Cirqus.Snapshotting;
+using d60.Cirqus.Tests.MongoDb;
+using NUnit.Framework;
+
+namespace d60.Cirqus.Tests.Snapshotting
+{
+    [TestFixture]
+    public class TestCachingAggregateRootRepositoryDecorator : FixtureBase
+    {
+        CachingAggregateRootRepositoryDecorator _cachingAggregateRootRepository;
+        MongoDbEventStore _eventStore;
+        JsonDomainEventSerializer _domainEventSerializer;
+        DefaultAggregateRootRepository _realAggregateRootRepository;
+        DefaultDomainTypeNameMapper _domainTypeNameMapper;
+
+        protected override void DoSetUp()
+        {
+            _eventStore = new MongoDbEventStore(MongoHelper.InitializeTestDatabase(), "Events");
+            _domainEventSerializer = new JsonDomainEventSerializer();
+
+            _domainTypeNameMapper = new DefaultDomainTypeNameMapper();
+            
+            var snapshotCache = new InMemorySnapshotCache();
+
+            _realAggregateRootRepository = new DefaultAggregateRootRepository(_eventStore, _domainEventSerializer, _domainTypeNameMapper);
+            _cachingAggregateRootRepository = new CachingAggregateRootRepositoryDecorator(_realAggregateRootRepository, snapshotCache, _eventStore, _domainEventSerializer);
+        }
+
+        [TestCase(1000, true)]
+        [TestCase(1000, false)]
+        public void CheckHydrationPerformance(int historyLength, bool useCaching)
+        {
+            var aggregateRootId = Guid.NewGuid();
+
+            GeneratePrettyLongHistory(aggregateRootId, historyLength);
+
+            TakeTime("Load instance", () =>
+            {
+                100.Times(() =>
+                {
+                    var realUnitOfWork = new RealUnitOfWork(_cachingAggregateRootRepository, _domainTypeNameMapper);
+
+                    if (useCaching)
+                    {
+                        _cachingAggregateRootRepository.Get<Root>(aggregateRootId.ToString(), realUnitOfWork);
+                    }
+                    else
+                    {
+                        _realAggregateRootRepository.Get<Root>(aggregateRootId.ToString(), realUnitOfWork);
+                    }
+                });
+            });
+        }
+
+        void GeneratePrettyLongHistory(Guid aggregateRootId, int howLong)
+        {
+            var eventsToSave = Enumerable.Range(0, howLong)
+                .Select(i => CreateEvent(aggregateRootId, i))
+                .Select(e => _domainEventSerializer.Serialize(e))
+                .ToList();
+
+            var random = new Random();
+
+            foreach (var partition in eventsToSave.Batch(100))
+            {
+                var batchSize = random.Next(10)+1;
+
+                foreach (var batch in partition.Batch(batchSize))
+                {
+                    _eventStore.Save(Guid.NewGuid(), batch);
+                }
+            }
+        }
+
+        DomainEvent CreateEvent(Guid aggregateRootId, int sequenceNumber)
+        {
+            return new Event
+            {
+                Meta =
+                {
+                    {DomainEvent.MetadataKeys.AggregateRootId, aggregateRootId.ToString()},
+                    {DomainEvent.MetadataKeys.SequenceNumber, sequenceNumber.ToString()},
+                    {DomainEvent.MetadataKeys.GlobalSequenceNumber, sequenceNumber.ToString()},
+                    {DomainEvent.MetadataKeys.Owner, _domainTypeNameMapper.GetName(typeof(Root))},
+                    {DomainEvent.MetadataKeys.Type, _domainTypeNameMapper.GetName(typeof(Event))},
+                }
+            };
+        }
+
+        class Root : AggregateRoot, IEmit<Event>
+        {
+            public int NumberOfProcessedEvents { get; private set; }
+
+            public void Apply(Event e)
+            {
+                NumberOfProcessedEvents++;
+            }
+        }
+
+        class Event : DomainEvent<Root> { }
+    }
+}
--- d60.Cirqus/Events/DomainEvent.cs
+++ d60.Cirqus/Events/DomainEvent.cs
@@ -12,15 +12,58 @@ namespace d60.Cirqus.Events
         /// </summary>
         public static class MetadataKeys
         {
+            /// <summary>
+            /// Gobal sequence of number that must be globally unique and monotonically increasing with each event in the event store
+            /// </summary>
             public const string GlobalSequenceNumber = "gl_seq";
+
+            /// <summary>
+            /// Sequence number local to an aggregate root instance
+            /// </summary>
             public const string SequenceNumber = "seq";
+
+            /// <summary>
+            /// The ID of the aggregate root instance that emitted the event
+            /// </summary>
             public const string AggregateRootId = "root_id";
+
+            /// <summary>
+            /// ID of the event batch within which this event is contained. Might/might not be logically enforced by the database.
+            /// </summary>
             public const string BatchId = "batch_id";
+
+            /// <summary>
+            /// UTC time of when the event was emitted
+            /// </summary>
             public const string TimeUtc = "time_utc";
+
+            /// <summary>
+            /// "Owner" of the event - must be set to the type name of the aggregate root that emitted this event
+            /// </summary>
             public const string Owner = "owner";
+
+            /// <summary>
+            /// Type of the event - must be set to the name of the event type
+            /// </summary>
             public const string Type = "type";
+
+            /// <summary>
+            /// Indicates the version number of the root that emitted this event. Can be used in migration scenarios where events emitted by and
+            /// aggregate root in a specific version might be treated slightly differently. 
+            /// Will be automatically applied to events when the aggregate root class is decorated with the <see cref="MetaAttribute"/> with <see cref="RootVersion"/> as key.
+            /// </summary>
             public const string RootVersion = "root_ver";
+
+            /// <summary>
+            /// Indicates the version number of the event when it was emitted. Can be used in migration scenarios where events emitted by and
+            /// aggregate root in a specific version might be treated slightly differently
+            /// Will be automatically applied to events when the event class is decorated with the <see cref="MetaAttribute"/> with <see cref="EventVersion"/> as key.
+            /// </summary>
             public const string EventVersion = "evt_ver";
+
+            /// <summary>
+            /// If enabled: The name of the type of command that caused this event
+            /// </summary>
             public const string CommandTypeName = "command_type";
         }
 
