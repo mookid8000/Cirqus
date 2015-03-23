57702ec9733eba0ec0217683849f4d5249d37b43
Mogens Heller Grabe <mookid8000@gmail.com>
2015-03-23 17:22:50 +0100

    fixed test

--- d60.Cirqus.Tests/Views/TestLoadAggregatesFromView.cs
+++ d60.Cirqus.Tests/Views/TestLoadAggregatesFromView.cs
@@ -79,7 +79,7 @@ namespace d60.Cirqus.Tests.Views
             Console.WriteLine("---------------------------------------------");
 
             Assert.That(stringWithTheLines.ToLowerInvariant(), Contains.Substring("exception"));
-            Assert.That(stringWithTheLines.ToLowerInvariant(), Contains.Substring("does not exist"));
+            Assert.That(stringWithTheLines.ToLowerInvariant(), Contains.Substring("but it didn't exist"));
         }
 
         [Test]
