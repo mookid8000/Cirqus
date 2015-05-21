bb012054c7e2a70d23ae0e5c39b1d72ee119e1ba
Matej Hertis <matej@nomnio.com>
2015-05-21 10:10:25 +0200

    Revert "Revert "MSTest integration""
    This reverts commit 7ba107f2101b797723ad4a776118b324a5a72a49.

---
+++ d60.Cirqus.MSTest/CirqusTests.cs
@@ -0,0 +1,47 @@
+﻿#region
+
+using d60.Cirqus.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+
+#endregion
+
+namespace d60.Cirqus.MSTest
+{
+    public class CirqusTests : CirqusTestsHarness
+    {
+        public TestContext TestContext { get; protected set; }
+
+        [TestInitialize]
+        public void SetupInternal()
+        {
+            Begin(CreateContext());
+            Setup();
+        }
+
+        protected virtual Testing.TestContext CreateContext()
+        {
+            return Testing.TestContext.Create();
+        }
+
+        protected virtual void Setup()
+        {
+        }
+
+        [TestCleanup]
+        public void TeardownInternal()
+        {
+            Teardown();
+            End(TestContext.CurrentTestOutcome == UnitTestOutcome.Error);
+        }
+
+        protected virtual void Teardown()
+        {
+        }
+
+        protected override void Fail()
+        {
+            Assert.Fail();
+        }
+    }
+}
