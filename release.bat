7d1dd088b659b10d50f0a67828c42b497a18cf66
Mogens Heller Grabe <mookid8000@gmail.com>
2017-06-15 07:29:01 +0200

    update release script

--- release.bat
+++ release.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 
+SETLOCAL EnableDelayedExpansion
+
 IF "%1" == "" (
 	ECHO Please specify the version to release like this:
 	ECHO.
@@ -8,13 +10,37 @@ IF "%1" == "" (
 	GOTO EXIT	
 )
 
+ECHO Building and publishing packages
+msbuild build.proj /t:release_packages /p:Version=%1
+IF !ERRORLEVEL! NEQ 0 (
+	ECHO Could not build.
+	GOTO FAIL
+)
+
+EXIT /B
+
+PAUSE
+
 ECHO Creating git tag %1
 git tag %1
-
-ECHO Building & publishing packages
-msbuild build.proj /t:release_packages /p:Version=%1
+IF !ERRORLEVEL! NEQ 0 (
+	ECHO Could not create tag.
+	GOTO FAIL
+)
 
 ECHO Pushing tags
 git push --tags
+IF !ERRORLEVEL! NEQ 0 (
+	ECHO Could not push tag.
+	GOTO FAIL
+)
 
 :EXIT
+GOTO SUCCESS
+
+:FAIL
+ECHO An error occurred.
+EXIT /B 1
+
+
+:SUCCESS
